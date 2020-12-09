8b2a81dfd96ea6466c4071a62ab1143e57bcc767
sanjadoc <sanja_doc@ukr.net>
2020-12-10 01:25:13 +0200

    add(env) config file and use(err) change code

--- api/server.js
+++ api/server.js
@@ -1,12 +1,28 @@
-var express = require('express');
-var app = express();
-const port = 3000;
+require('dotenv').config();
+const host = process.env.HOST;
+const port = process.env.PORT;
 
-app.get('/:name',function(req, res) {
-    res.send('Hello ' + req.params.name+ '!');
+const express = require('express');
+const app = express();
+
+
+app.get('/', function(req, res) {
+    res.send('Hello on my education express page!');
 });
+
+// not need now
+// app.get('/:name', function(req, res) {
+//     res.send('Hello ' + req.params.name+ '!');
+// });
+
+app.use( function(err, res) {
+    console.log(err.stack);
+    res.status(500).send('Oh sorry, server say - error 500');
+})
 
 app.listen(port, ()=> {
-    console.log(`App server started. App listening at http://localhost:${port}`);
+    console.log(`App server started. App listening at http://${host}:${port}`);
 });
+
+
 
