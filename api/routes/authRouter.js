8f4393c6844fd91d3380744076a344e7fea90835
sanjadoc <sanja_doc@ukr.net>
2021-03-07 22:13:26 +0200

    add data validtor

--- api/routes/authRouter.js
+++ api/routes/authRouter.js
@@ -1,13 +1,22 @@
 const router = require("express").Router();
 const authController = require("../controllers/authController");
+const validator = require("../middleware/validator");
 
-router.post("/login", authController.login);
-router.post("/registration", authController.registration);
+router.post("/login",[
+    validator({
+        email: ["required", "email"],
+        password: ["required"],
+    }), authController.login]);
+
+router.post("/registration",[
+    validator({
+        email: ["required", "email", "unique:users:email"],
+        password: ["required", "min:8", "max:50"],
+    }), authController.registration]);
 router.get("/verify/:token", authController.emailVerify);
 router.get("/logout", authController.logout);
 
 router.post("/social/google", authController.google);
-
 router.post("/social/facebook", authController.facebook);
 
 module.exports = router;
